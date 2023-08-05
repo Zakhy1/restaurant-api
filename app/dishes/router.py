c2bfd963da3b8d1f18b88b8debe849c84f73d642
zakhy <lirchzakhar@gmail.com>
2023-08-05 21:47:09 +0700

    add typehints for routers

--- app/dishes/router.py
+++ app/dishes/router.py
@@ -1,36 +1,36 @@
 from fastapi import APIRouter
 
-from app.dishes.schemas import DishSchema
+from app.dishes.schemas import DishSchema, DishSchemaResponse
 from app.dishes.service import DishService
 
 dish_router = APIRouter()
 
 
 @dish_router.get("/dishes")
-def get_dishes(menu_id: int, submenu_id: int):
+def get_dishes(menu_id: int, submenu_id: int) -> list[DishSchemaResponse]:
     service = DishService()
     return service.get_dishes(menu_id, submenu_id)
 
 
 @dish_router.get("/dishes/{dish_id}")
-def get_dish(menu_id: int, submenu_id: int, dish_id: int):
+def get_dish(menu_id: int, submenu_id: int, dish_id: int) -> DishSchemaResponse:
     service = DishService()
     return service.get_dish(menu_id, submenu_id, dish_id)
 
 
 @dish_router.post("/dishes", status_code=201)
-def add_dish(menu_id: int, submenu_id: int, dish: DishSchema):
+def add_dish(menu_id: int, submenu_id: int, dish: DishSchema) -> DishSchemaResponse:
     service = DishService()
     return service.add_dish(menu_id, submenu_id, dish)
 
 
 @dish_router.patch("/dishes/{dish_id}")
-def edit_dish(menu_id: int, submenu_id: int, dish_id: int, dish: DishSchema):
+def edit_dish(menu_id: int, submenu_id: int, dish_id: int, dish: DishSchema) -> DishSchemaResponse:
     service = DishService()
     return service.edit_dish(menu_id, submenu_id, dish_id, dish)
 
 
 @dish_router.delete("/dishes/{dish_id}")
-def delete_dish(menu_id: int, submenu_id: int, dish_id: int):
+def delete_dish(menu_id: int, submenu_id: int, dish_id: int) -> dict:
     service = DishService()
     return service.delete_dish(menu_id, submenu_id, dish_id)
--- app/submenu/router.py
+++ app/submenu/router.py
@@ -1,36 +1,36 @@
 from fastapi import APIRouter
 
-from app.submenu.schemas import SubMenuSchema
+from app.submenu.schemas import SubMenuSchema, SubMenuSchemaResponse
 from app.submenu.service import SubMenuService
 
 submenu_router = APIRouter()
 
 
 @submenu_router.get("/submenus")
-def get_submenus(menu_id: int):
+def get_submenus(menu_id: int) -> list[SubMenuSchemaResponse]:
     service = SubMenuService()
     return service.get_submenus(menu_id)
 
 
 @submenu_router.get("/submenus/{submenu_id}")
-def get_submenu(menu_id, submenu_id):
+def get_submenu(menu_id: int, submenu_id: int) -> SubMenuSchemaResponse:
     service = SubMenuService()
     return service.get_submenu(menu_id, submenu_id)
 
 
 @submenu_router.post("/submenus", status_code=201)
-def add_submenu(menu_id, submenu: SubMenuSchema):
+def add_submenu(menu_id, submenu: SubMenuSchema) -> SubMenuSchemaResponse:
     service = SubMenuService()
     return service.add_submenu(menu_id, submenu)
 
 
 @submenu_router.patch("/submenus/{submenu_id}")
-def edit_submenu(menu_id, submenu_id, submenu: SubMenuSchema):
+def edit_submenu(menu_id, submenu_id, submenu: SubMenuSchema) -> SubMenuSchemaResponse:
     service = SubMenuService()
     return service.edit_submenu(menu_id, submenu_id, submenu)
 
 
 @submenu_router.delete("/submenus/{submenu_id}")
-def delete_submenu(menu_id: int, submenu_id: int):
+def delete_submenu(menu_id: int, submenu_id: int) -> dict:
     service = SubMenuService()
     return service.delete_submenu(menu_id, submenu_id)
